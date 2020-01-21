08230535ad133f4d0aafe83a6309b9119bfaafc2
Fabien Billault <fabien.billault@gmail.com>
2020-01-22 00:53:34 +0100

    Final version

--- js/script.js
+++ js/script.js
@@ -1,33 +1,176 @@
 (function(){
 
-    let lastOperationResult = 0;
+    let input = {
+            firstValue:'',
+            operator:'',
+            secondValue:''
+        },
+        operationResult = '';
 
-    function handleKeyAction(keyCode)
+    function clearEvent(value)
     {
-        const screen = document.getElementById('calculatorScreen');
+        input[value] = input[value].substring(0, input[value].length - 1);
+        operationResult = '';
+    }
 
-        let enteredValues = [],
-            choosenOperator = '';
-        
-        let value = String.fromCharCode(keyCode);
-
-        if (value === 'c')
+    function dotEvent(value)
+    {
+        if (input[value].indexOf('.') === -1)
         {
-            if (enteredValues)
+            if (!input[value] || input[value] === '-') { input[value] += '0'; }
+
+            input[value] += '.';
+        }
+    }
+
+    function equalEvent()
+    {
+        if (!isNaN(parseInt(input.secondValue)))
+        {
+            executeOperation();
+        }
+    }
+
+    function operatorEvent(value, operator)
+    {
+        if (!input[value])
+        {
+            if (operator === '-' && input.operator != '-' && !operationResult)
             {
-                enteredValues = [];
+                input[value] += '-';
             }
             else
             {
-                lastOperationResult = 0;
+                if (value === 'firstValue')
+                {
+                    if (operationResult)
+                    {
+                        input.firstValue = operationResult;
+                        operationResult = '';
+                    }
+                    else
+                    {
+                        input.firstValue = '0';
+                    }
+                }
+
+                input.operator = operator;
             }
         }
-        else if (value === '=')
+        else
         {
+            if (input[value] === '-' && operator === '+')
+            {
+                input[value] = '';
+            }
+            else if (!isNaN(input[value]))
+            {
+                input[value] = parseFloat(input[value]);
+                
+                if (value === 'firstValue')
+                {
+                    input.operator = operator;
+                }
+                else if (value === 'secondValue')
+                {
+                    executeOperation(operator);
+                }
+            }
+        }
+    }
 
+    function executeOperation(nextOperator = '')
+    {
+        const operation = {
+            '/': function(a, b) { return a / b; },
+            '*': function(a, b) { return a * b; },
+            '+': function(a, b) { return a + b; },
+            '-': function(a, b) { return a - b; }
+        };
+
+        operationResult = operation[input.operator](parseFloat(input.firstValue), parseFloat(input.secondValue));
+        operationResult = +(operationResult).toFixed(7);
+        
+        if (nextOperator)
+        {
+            input.firstValue = operationResult;
+            operationResult = '';
+            input.operator = nextOperator;
+        }
+        else
+        {
+            input.firstValue = input.operator = '';
         }
 
-        console.log();
+        input.secondValue = '';
+    }
+
+    function writeOnScreen()
+    {
+        const screen  = document.getElementById('calculatorScreen');
+        let   content;
+        
+        if (!isNaN(parseInt(operationResult, 10)))
+        {
+            content = operationResult.toString();
+        }
+        else
+        {
+            let outputOperator = input.operator;
+
+            if (outputOperator === '*')
+            {
+                outputOperator = '×';
+            }
+            else if (outputOperator === '/')
+            {
+                outputOperator = '÷';
+            }
+
+            content = `${input.firstValue} ${outputOperator} ${input.secondValue}`;
+        }
+
+        content.trim() ? screen.textContent = content.trim() : screen.textContent = '0';
+    }
+
+    function handleKeyAction(keyCode)
+    {
+        let newInput = String.fromCharCode(keyCode),
+            currentValue = !input.operator ? 'firstValue' : 'secondValue';
+
+        switch(newInput)
+        {
+            case 'c':
+                clearEvent(currentValue);
+                break;
+            case '.':
+                dotEvent(currentValue);
+                break;
+            case '=':
+                equalEvent();
+                break;
+            case '/':
+            case '*':
+            case '+':
+            case '-':
+                operatorEvent(currentValue, newInput);
+                break;
+            default:
+                if (!isNaN(parseInt(newInput, 10)))
+                {
+                    input[currentValue] += newInput;
+                    operationResult = '';
+                }
+        }
+
+        writeOnScreen();
+
+        /********************* DEBUG **************************/
+        let str = '';
+        for (element in input) { str += `${element} : ${input[element]}\n`; }
+        str += `operationResult : ${operationResult}`;
+        console.log(str);
+        /********************* DEBUG **************************/
     }
 
     const keyElts = [...document.getElementsByClassName('key')];
